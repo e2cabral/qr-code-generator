3fc21c03dfc009d65e6d1c418aa2ae1516d73409
Edson Cabral <edson.cabral@trescon.com.br>
2020-09-02 19:39:38 -0300

    💄 create a header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <div className="header__left">
+
+            </div>
+            <div className="header__right">
+
+            </div>
+        </div>
+    );
+}
