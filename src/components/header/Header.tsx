059671f71acf0dc6cb963ead199792e651df08a1
Edson Cabral <edson.cabral@trescon.com.br>
2020-09-02 20:19:35 -0300

    💄 add layout configuration to header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
+import './index.css';
+
 export const Header = () => {
     return (
         <div className="header">
             <div className="header__left">
-
+                <p className="header__logo">
+                    <i className="icofont-qr-code"></i>
+                    <span className="header__logoText">
+                        QrCode <strong>Generator</strong>
+                    </span>
+                </p>
             </div>
             <div className="header__right">
-
+                {/* TODO: implement a menu in the right side */}
             </div>
         </div>
     );
