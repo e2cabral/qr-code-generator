01b6d0a6bb1783029058b95a1b5a23f82db3e7ba
Edson Cabral <edson.cabral@trescon.com.br>
2020-09-02 21:03:37 -0300

    💄 add components to qr code component

--- src/pages/qr-code-generator/QrCodeGenerator.tsx
+++ src/pages/qr-code-generator/QrCodeGenerator.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
 import { Header } from '../../components/header/Header';
+import { Container } from '../../components/layout/container/Container';
+import { Row } from '../../components/layout/row/Row';
+import { Column } from '../../components/layout/column/Column';
 
 export const QrCodeGenerator = () => {
     return (
         <div className="qrCodeGenerator">
             <Header />
+            <Container>
+                <Row>
+                    <Column>
+                        <p>
+                            Mussum Ipsum, cacilds vidis litro abertis. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Quem num gosta di mim que vai caçá sua turmis! Atirei o pau no gatis, per gatis num morreus. Quem manda na minha terra sou euzis!
+                            Sapien in monti palavris qui num significa nadis i pareci latim. Paisis, filhis, espiritis santis. Casamentiss faiz malandris se pirulitá. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.
+                            Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.
+                            Si num tem leite então bota uma pinga aí cumpadi! Interagi no mé, cursus quis, vehicula ac nisi. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Cevadis im ampola pa arma uma pindureta. 
+                        </p>
+                    </Column>
+                    <Column>
+                        <p>
+                            Mussum Ipsum, cacilds vidis litro abertis. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Quem num gosta di mim que vai caçá sua turmis! Atirei o pau no gatis, per gatis num morreus. Quem manda na minha terra sou euzis!
+                            Sapien in monti palavris qui num significa nadis i pareci latim. Paisis, filhis, espiritis santis. Casamentiss faiz malandris se pirulitá. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.
+                            Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.
+                            Si num tem leite então bota uma pinga aí cumpadi! Interagi no mé, cursus quis, vehicula ac nisi. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Cevadis im ampola pa arma uma pindureta. 
+                        </p>
+                    </Column>
+                </Row>
+            </Container>
         </div>
     );
 }
