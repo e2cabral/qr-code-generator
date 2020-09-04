f0b2110712e7f1ce2c2349c48ae34323835a4634
Edson Cabral <edson.cabral@trescon.com.br>
2020-09-04 20:08:01 -0300

    🚧 add constructor to QrCode class

--- src/models/QrCode.ts
+++ src/models/QrCode.ts
@@ -1,4 +1,12 @@
-export class QrCode {
+export abstract class QrCode {
+    constructor(value: string, bgColor: string, fgColor: string, size: number, level: 'L' | 'M' | 'Q' | 'H') {
+        this.value = value;
+        this.bgColor = bgColor;
+        this.fgColor = fgColor;
+        this.size = size;
+        this.level = level;
+    }
+
     value: string;
     bgColor: string;
     fgColor: string;
